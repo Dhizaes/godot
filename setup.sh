9f82e149cccc851e16356dd2a2975c966904b042
Nokklair <nokklair@proton.me>
2025-11-27 20:11:27 +0300

    Add debug symbols and debug optimisation to the setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-SCONSFLAGS="-j8" scons platform=linuxbsd use_llvm=yes linker=lld precision=double num_jobs=8
+SCONSFLAGS="-j8" scons platform=linuxbsd use_llvm=yes linker=lld debug_symbols=yes optimize=debug precision=double num_jobs=8
