536ede39300fd5948e6ca87353084b3ad7a79fc5
nokklair <nokklair@proton.me>
2026-01-11 15:23:11 +0300

    Enable Dev Build, Mono, and disable deprecated
    Signed-off-by: nokklair <nokklair@proton.me>

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-SCONSFLAGS="-j8" scons platform=linuxbsd use_llvm=yes linker=lld debug_symbols=yes optimize=debug precision=double num_jobs=8
+SCONSFLAGS="-j8" scons platform=linuxbsd use_llvm=yes linker=lld dev_build=yes debug_symbols=yes optimize=debug precision=double module_mono_enabled=yes deprecated=no num_jobs=8
+
+./bin/godot.linuxbsd.editor.dev.double.x86_64.llvm.mono --headless --generate-mono-glue modules/mono/glue
+
+./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --no-deprecated --precision=double
